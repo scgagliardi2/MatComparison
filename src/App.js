41424b4557f7fe62ddb0b16a2d437406ef76dcc0
Samuel Gagliardi <Samuel.Gagliardi@cohu.com>
2020-01-14 13:34:53 -0600

    small changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="Cohu.png" alt="test"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the Cohu Materials Database
         </p>
         <a
           className="App-link"
