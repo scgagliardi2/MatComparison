a2b259113db4feba81be61d4fc772fccef05b194
Samuel Gagliardi <Samuel.Gagliardi@cohu.com>
2020-01-24 10:48:40 -0600

    set up header and base display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Logo from "./Cohu-Standard-Color-Logo-Transpaprent.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Cohu.png" alt="test"/>
-        <p>
-          Welcome to the Cohu Materials Database
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a href="https://www.cohu.com/" target="_blank">
+          <img className="Header-logo" src={Logo} alt="image not found" />
         </a>
+        <p className="Header-name">Cohu Materials Database</p>
       </header>
+      <body></body>
     </div>
   );
 }
